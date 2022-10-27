e5692ba08497798ae4d71ce95b20308e5d650363
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-10-27 19:30:53 +0530

    added HomePage

--- Submissiva/src/app/app-routing.module.ts
+++ Submissiva/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
+import { HomePageComponent } from './Component/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Submissiva/src/app/app.component.ts
+++ Submissiva/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Submissiva';
+  constructor() {}
+  goToDashboard() {}
+  login_btn() {}
+  register_btn() {}
+  logout_btn() {}
 }
