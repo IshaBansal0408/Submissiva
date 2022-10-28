4238dfb4242dcc2cae0cd20c66366dd09b5f024f
IshaBansal0408 <isha.bansal1504@gmail.com>
2022-10-29 00:49:58 +0530

    made a few changes before deployment

--- Submissiva/src/environments/environment.prod.ts
+++ Submissiva/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDsU5I8njiYZ7bAD1uxTbnuK3FnNXqkvOs',
+    authDomain: 'idea-submission-application.firebaseapp.com',
+    projectId: 'idea-submission-application',
+    storageBucket: 'idea-submission-application.appspot.com',
+    messagingSenderId: '888224823090',
+    appId: '1:888224823090:web:5f25f72db31b4a9e63c92b',
+    measurementId: 'G-HYHV3VNW3F',
+  },
 };
